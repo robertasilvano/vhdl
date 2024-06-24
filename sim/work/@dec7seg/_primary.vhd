f4d87f374be77d144b99bdd0f54ece3f50fbb7c0
Roberta Silvano <robertaspb@gmail.com>
2024-06-24 19:56:48 -0300

    Revert "reorganizando pastas"
    This reverts commit a7a70de9609b2bd31fc86784bc0b6038df0dd9af.

--- sim/work/@dec7seg/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity Dec7seg is
-    port(
-        G               : in     vl_logic_vector(3 downto 0);
-        O               : in     vl_logic_vector(6 downto 0)
-    );
-end Dec7seg;
