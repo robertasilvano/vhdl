568c55b284e02e7e149badea2b9b1f34e5b1d29a
Roberta Silvano <robertaspb@gmail.com>
2024-06-24 19:13:52 -0300

    Revert "ajustando organizacao das pastas"
    This reverts commit fd957b2433a33913215d28e693ece486585b24f4.

--- sim/work/@dec@seq/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity DecSeq is
-    port(
-        address         : in     vl_logic_vector(3 downto 0);
-        \out\           : in     vl_logic_vector(3 downto 0)
-    );
-end DecSeq;
