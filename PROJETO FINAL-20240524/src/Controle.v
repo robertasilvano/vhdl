2daf95febab3b911d62d710cd59724b67d9f0eb1
July Chassot <51218070+LastChassot@users.noreply.github.com>
2024-06-24 17:15:48 -0300

    Update Controle.v
    Mudanças da aula do dia 14/06

--- PROJETO FINAL-20240524/src/Controle.v
+++ PROJETO FINAL-20240524/src/Controle.v
@@ -24,14 +24,89 @@ sel
 	input wire 		end_time;
 	input wire 		win;
 	input wire 		match;
-	output wire 	r1;
-	output wire 	r2;
-	output wire 	e1;
-	output wire 	e2;
-	output wire 	e3;
-	output wire 	e4;
-	output wire 	sel;
-	
-	assign r2 = reset;  //maracutaia só pra conseguir testar
 
+	output reg 		r1, r2;
+	output reg 		e1, e2, e3, e4;
+	output reg 		sel;
+	
+	//assign r2 = reset;  //maracutaia só pra conseguir testar
+
+localparam N_STADOS = 3;
+localparam [2:0]
+Init = 3'b000, 
+Setup = 3'b001, 
+Play_FPGA = 3'b010, 
+Play_User = 3'b011, 
+Check = 3'b100, 
+Next_Round = 3'b101, 
+Result = 3'b110; 
+
+reg [N_STADOS-1:0]state
+next_state;
+
+// Processo sequencial
+// Armazena estado ATUAL
+always @(posedge clock_50)
+begin
+	if (reset)
+		state <= Init;
+	else 
+		state <= next_state;
+end
+
+
+// LOGICA COMBINACIONAL DOS ESTADOS
+always @(end_fpga or end_user or end_time or win or match or state) 
+begin	
+	next_state = state;
+	case (state)
+		Init: 
+		begin
+			next_state = Setup;
+		end
+		Setup: 
+			if (enter == 1'b1) next_state = Play_FPGA;
+		Play_FPGA: 
+			if (end_fpga == 1'b1) next_state = Play_User;
+		Play_User: 
+		begin
+			if (end_time == 1'b1) next_state = Result;
+			else if (end_user = 1'b1 )next_state = Check;
+		end
+		Check:
+		begin 
+		  	if (match == 1'b1) next_state = Next_Round;
+			else next_state =  Result;
+		end
+		Next_Round: 
+		begin
+			if (win) next_state = Result;
+			else next_state = Play_FPGA
+		end
+		Result: next_state = Init;
+
+	endcase
+end 
+// FIM LOGICA COMBINACIONAL DE ESTADOS
+
+// LOGICA COMBINACIONAL CONTROLE DAS SAIDAS
+always @(state) begin
+	r1 = 1'b1; r2 = 1'b1;
+	e1 = 1'b0; e2 = 1'b0; e3 = 1'b0; e4 = 1'b0;
+	sel = 1'b0;
+	case (state)
+		Init: 
+			begin
+			r1 = 1'b1; r2 = 1'b1;
+			end
+		Setup: e1 = 1'b1;
+		Play_FPGA: e3 = 1'b1;
+		Play_User: e2 = 1'b1; 
+		Check: e4 = 1'b1;
+		Next_Round: r2 = 1'b1;
+		Result: sel = 1'b1;
+
+	endcase
+end
+	
 endmodule
