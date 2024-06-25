41fc7238528868e2e42a2083e3889ead5780b653
Roberta Silvano <robertaspb@gmail.com>
2024-06-25 00:02:49 -0300

    add counter_round

--- PROJETO FINAL-20240524/src/Counter_round.v
+++ PROJETO FINAL-20240524/src/Counter_round.v
@@ -1,3 +1,15 @@
+/*
+Contar a quantidade de sequências que o jogador acertou em um jogo
+Entradas:
+	- data: determina o número total de rodadas do jogo
+	- clk: para sincronização
+	- R: reset
+	- E: habilitar a contagem
+Saidas:
+	- tc: indica se o jogador ganhou o jogo
+	- round: quantidade de sequências que o jogador acertou
+*/
+
 module Counter_round(
 	//entrada de dados
 	clk,
@@ -6,12 +18,13 @@ module Counter_round(
 	data,
 	
 	//saída de dados
-	tc
+	tc,
+	round
 );
 
 	//localparams
 	localparam p_data = 4;
-	localparam p_q = 4;  //ver tamananho
+	localparam p_round = 4;  //ver tamananho
 
 	// Input Port(s)
 	input wire 						clk;
@@ -20,5 +33,28 @@ module Counter_round(
 	input wire [p_data - 1:0] 	data;
 	
 	// Output Port(s)
-	output wire [p_q - 1:0]		tc;  //wire ou reg?
+	output reg 							tc;
+	output reg [p_round - 1:0]		round;
+	
+	always @(posedge clk or R)  // perguntar pro prof. R não tem posedge ne?
+	begin
+		if (R == 1'b1)
+			begin
+				round <= 4'b0000;
+				tc <= 1'b0;
+			end
+		else
+			begin
+				if (E == 1'b1)
+					begin
+						round <= round + 1'b1;
+					end
+				
+				if (round == data)  //ganhou
+					begin
+						tc <= 1'b1;
+					end
+			end
+		
+	end
 endmodule
